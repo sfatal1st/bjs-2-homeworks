81919580e46f8da999fbca828e7c2522c3a61423
Andrey <symonany@gmail.com>
2022-08-13 21:31:29 +0300

    Two variant

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,6 @@
 function solveEquation(a, b, c) {
     "use strict";
-    let arr;
+    let arr = [];
     // код для задачи №1 писать здесь
     let rootEquation;
     let discriminant = b**2 - 4*a*c;
@@ -9,10 +9,10 @@ function solveEquation(a, b, c) {
         rootEquation = -b / (2*a);
         arr.push(rootEquation);
     }
-    else if (discriminant < 0) {
-        rootEquation = (-b + Math.sqrt(d) )/(2*a);
+    else if (discriminant > 0) {
+        rootEquation = (-b + Math.sqrt(discriminant) )/(2*a);
         arr.push(rootEquation);
-        rootEquation = (-b - Math.sqrt(d) )/(2*a);
+        rootEquation = (-b - Math.sqrt(discriminant) )/(2*a);
         arr.push(rootEquation);
     }
     return arr; // array
