7d2bd1c802b6a3c2e09c3c7e1a3b6d22be8e74f6
Andrey <symonany@gmail.com>
2022-08-13 21:01:02 +0300

    First variant

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,27 @@
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+    "use strict";
+    let arr;
+    // код для задачи №1 писать здесь
+    let rootEquation;
+    let discriminant = b**2 - 4*a*c;
 
+    if (discriminant === 0) {
+        rootEquation = -b / (2*a);
+        arr.push(rootEquation);
+    }
+    else if (discriminant < 0) {
+        rootEquation = (-b + Math.sqrt(d) )/(2*a);
+        arr.push(rootEquation);
+        rootEquation = (-b - Math.sqrt(d) )/(2*a);
+        arr.push(rootEquation);
+    }
+    return arr; // array
+  }
+  
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
+    let totalAmount;
+   
+    // код для задачи №2 писать здесь
+   
+    return totalAmount;
 }
