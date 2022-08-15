8b0e961aadf544e66e8fc067bd54bfb37bcf88f9
Andrey <symonany@gmail.com>
2022-08-15 13:10:50 +0300

    Three variant

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -19,9 +19,24 @@ function solveEquation(a, b, c) {
   }
   
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    let totalAmount;
-   
+    "use strict";
+    let totalAmount, bodyCredit, termCredit, percentMonth, payMonth;
+    let today = new Date();
     // код для задачи №2 писать здесь
-   
-    return totalAmount;
+    if (isNaN(parseFloat(percent)) && !isFinite(percent)) {
+        return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+    }
+    if (isNaN(parseFloat(contribution)) && !isFinite(contribution)) {
+        return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;    
+    }
+    if (isNaN(parseFloat(amount)) && !isFinite(amount)) {
+        return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+    }
+    bodyCredit = amount - contribution;
+    percent = percent / 100;
+    percentMonth = percent / 12;
+    termCredit = date.getMonth() - today.getMonth() + (12 * (date.getFullYear() - today.getFullYear()));
+    payMonth = bodyCredit * (percentMonth + (percentMonth / (Math.pow(1 + percentMonth, termCredit) - 1)));
+    totalAmount = payMonth * termCredit;
+    return Math.round(parseFloat(totalAmount) * 100) / 100;;
 }
