00d4e9de9e17ce71fcce6934b43139853bd6eea0
Andrey <symonany@gmail.com>
2022-08-17 19:31:23 +0300

    First variant

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,13 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  if (arr1.length > arr2.length) {
+    result = arr1.every((item, index) => item === arr2[index])
+  }
+  else {result = arr2.every((item, index) => item === arr1[index])};
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter(item => item > 0).filter(item => item % 3 === 0).map(item => item *10);
   return resultArr; // array
 }
